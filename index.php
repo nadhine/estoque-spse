e216d716c4c16b91bff20152fc930ee882d73a33
nadhine <hangrel@gmail.com>
2018-01-26 14:21:43 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,99 @@
+<?php
+//index.php
+include('database_connection.php');
+include('function.php');
+
+if(!isset($_SESSION["type"]))
+{
+	header("location:login.php");
+}
+
+include('header.php');
+
+?>
+	<br />
+	<div class="row">
+	<?php
+	if($_SESSION['type'] == 'master')
+	{
+	?>
+	<div class="col-md-3">
+		<div class="panel panel-default">
+			<div class="panel-heading"><strong>Número de usuários</strong></div>
+			<div class="panel-body" align="center">
+				<h1><?php echo count_total_user($connect); ?></h1>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="panel panel-default">
+			<div class="panel-heading"><strong>Numero de Categorias</strong></div>
+			<div class="panel-body" align="center">
+				<h1><?php echo count_total_category($connect); ?></h1>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="panel panel-default">
+			<div class="panel-heading"><strong>Número de Marcas</strong></div>
+			<div class="panel-body" align="center">
+				<h1><?php echo count_total_brand($connect); ?></h1>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="panel panel-default">
+			<div class="panel-heading"><strong>Total em estoque</strong></div>
+			<div class="panel-body" align="center">
+				<h1><?php echo count_total_product($connect); ?></h1>
+			</div>
+		</div>
+	</div>
+	<?php
+	}
+	?>
+		<div class="col-md-4">
+			<div class="panel panel-default">
+				<div class="panel-heading"><strong>Número das movimetações</strong></div>
+				<div class="panel-body" align="center">
+					<h1>$<?php echo count_total_order_value($connect); ?></h1>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="panel panel-default">
+				<div class="panel-heading"><strong>Valor total das movimetações</strong></div>
+				<div class="panel-body" align="center">
+					<h1>$<?php echo count_total_cash_order_value($connect); ?></h1>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="panel panel-default">
+				<div class="panel-heading"><strong>Total Credit Order Value</strong></div>
+				<div class="panel-body" align="center">
+					<h1>$<?php echo count_total_credit_order_value($connect); ?></h1>
+				</div>
+			</div>
+		</div>
+		<hr />
+		<?php
+		if($_SESSION['type'] == 'master')
+		{
+		?>
+		<div class="col-md-12">
+			<div class="panel panel-default">
+				<div class="panel-heading"><strong>Valor total do usuário</strong></div>
+				<div class="panel-body" align="center">
+					<?php echo get_user_wise_total_order($connect); ?>
+				</div>
+			</div>
+		</div>
+		<?php
+		}
+		?>
+	</div>
+
+<?php
+include("footer.php");
+?>
